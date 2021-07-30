194d1cad4453cdb4d55c62ba658f89e7e9f37fed
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2021-07-31 00:12:55 +0200

    `RESTEndpoint` now conforms to `Hashable`.

--- Sources/RESTAPIToolkit/officially_vended_products/rest_requests/RESTEndpoint.swift
+++ Sources/RESTAPIToolkit/officially_vended_products/rest_requests/RESTEndpoint.swift
@@ -6,7 +6,7 @@
 //
 
 ///
-public struct RESTEndpoint: Equatable,
+public struct RESTEndpoint: Hashable,
                             CustomStringConvertible,
                             Ergonomic {
     
