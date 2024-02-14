2ef6d70bb21b1ea29f80d8ec33910093ae33e208
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-02-14 15:24:51 +0100

    Fixed compilation error when building for watchOS.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 ///
 let package = Package(
     name: "RESTAPIToolkit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v4), .tvOS(.v11)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v5), .tvOS(.v11)],
     products: [
         
         ///
